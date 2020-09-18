38130fc4021cb822ff17ae88558cdb88d9997cd2
dushitaoyuan <dushitaoyuan>
2020-09-18 15:15:48 +0800

    example 添加独立打包配置

--- spring-vertx-example/src/main/java/com/taoyuanx/vertxdemo/nospring/NoSpringBootDemo.java
+++ spring-vertx-example/src/main/java/com/taoyuanx/vertxdemo/nospring/NoSpringBootDemo.java
@@ -1,17 +1,6 @@
 package com.taoyuanx.vertxdemo.nospring;
 
-import com.taoyuanx.springmvc.vertx.core.core.VertxConstant;
-import com.taoyuanx.springmvc.vertx.core.core.VertxHolder;
-import com.taoyuanx.springmvc.vertx.core.core.VertxHttpServerConfig;
-import com.taoyuanx.springmvc.vertx.core.core.template.AbstractTemplateEngineDelegate;
 import com.taoyuanx.springmvc.vertx.core.start.ServerBootStart;
-import com.taoyuanx.springmvc.vertx.core.start.VertxHttpServerVerticle;
-import io.vertx.core.VertxException;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
-import io.vertx.ext.web.templ.thymeleaf.ThymeleafTemplateEngine;
 
 
 public class NoSpringBootDemo {
